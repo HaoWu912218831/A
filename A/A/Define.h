da42b4463e6ad0d31bb09f396cb89481d2083cd8
912218831 <912218831@qq.com>
2019-01-16 21:58:46 +0800

    19

---
+++ A/A/Define.h
@@ -0,0 +1,16 @@
+//
+//  Define.h
+//  A
+//
+//  Created by 杨庆龙 on 2019/1/16.
+//  Copyright © 2019年 杨庆龙. All rights reserved.
+//
+
+#ifndef Define_h
+#define Define_h
+
+#define FONT(fontSize)                      [UIFont fontWithName:FONTNAME size:fontSize]
+#define ThickFont(fontSize)                 [UIFont fontWithName:ThickFontName size:fontSize]//加粗字体
+
+
+#endif /* Define_h */
